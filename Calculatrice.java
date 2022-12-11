379145a7b48927244b3fbb76f463ecb4aab9b25f
ngahadjo <fngahadjotchoua@ivytech.edu>
2022-12-10 18:48:34 -0600

    test

---
+++ Calculatrice.java
@@ -0,0 +1,33 @@
+package calculus;
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+
+
+public class Calculatrice extends JFrame {
+	private static final long serialVersionUID = 1L;
+
+	public Calculatrice(){
+		this.setSize(240, 260);
+		this.setTitle("Calculus");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
+		this.setResizable(false);
+		initComposant();
+		this.setVisible(true);
+	}
+
+	private void initComposant(){
+		Controleur controleur = new Controleur();
+		
+		Modele modele = new Modele();
+		View view = new View();
+		
+		controleur.setModele(modele);
+		controleur.setView(view);
+		
+		this.getContentPane().add(view, BorderLayout.NORTH);
+		this.getContentPane().add(controleur.getChiffre(), BorderLayout.CENTER);
+		this.getContentPane().add(controleur.getOperateur(), BorderLayout.EAST);
+	}
+}
